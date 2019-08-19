56c33e17cfd4acfe0259f764061a2b9b0e519475
mamartinod <ma.martinod@free.fr>
2019-08-19 18:04:37 +1000

    copy/paste functions into dedicated file

--- glint_simulator.py
+++ glint_simulator.py
@@ -8,52 +8,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import comb
 import glint_simulator_classes as classes
+from glint_simulator_functions import gaus, setZetaCoeff, save, rv_gen_doubleGauss
 from itertools import combinations
-import h5py
 import os
 import datetime
 from timeit import default_timer as time
 
-def gaus(x, mu, sigma):
-    y = np.exp(-(x - mu)**2/(2 * sigma**2))
-    y /= y.sum()
-    return y
 
-def chromatic_splitters(ratios, wl_scale, wl0, slope):
-    chroma_ratios = np.zeros((ratios.shape[0], 2, wl_scale.size))
-    chromatism = slope * (wl_scale - wl0)
-    chroma_ratios[:,0] = ratios[:,0,None] + chromatism[None,:]
-    chroma_ratios[:,1] = ratios[:,0,None] + chromatism[None,:]
-        
-    return chroma_ratios
-
-def save(arr, path, date, DIT, na, mag):
-    # Check if saved file exist
-    if os.path.exists(path):
-        opening_mode = 'w' # Overwright the whole existing file.
-    else:
-        opening_mode = 'a' # Create a new file at "path"
-        
-    with h5py.File(path, opening_mode) as f:
-        f.attrs['origin'] = 'sim'
-        f.attrs['date'] = date
-        f.attrs['nbimg'] = nbimg 
-        f.attrs['DIT'] = DIT
-        f.attrs['Na'] = na
-        f.attrs['mag'] = mag
-        f.create_dataset('imagedata', data=arr)
-
+    
 # =============================================================================
 # Settings
 # =============================================================================
 dark_only = False
-turbulence = False
+turbulence = True
 save_data = False
-nb_block = (0, 1)
-nbimg = 1
-select_noise = [False, False] # Dark and Readout noises
-conversion = False
-path = '/mnt/96980F95980F72D3/glint_data/simulation_nofluctu/'
+nb_block = (0, 10)
+nbimg = 6000
+select_noise = [True, True] # Dark and Readout noises
+conversion = True
+path = '/mnt/96980F95980F72D3/glint_data/simulation_opd0/'
 
 # =============================================================================
 # Fundamental constants
@@ -76,7 +49,7 @@ Ndark = 600 # in e-/px/s
 Ndark *= DIT # in e-/px/fr
 sigma_ron = 30. # in e-/px/fr
 
-wl_offset = 20 # in pixel, shift of the bandwidth on the detecotr hence a blank in a part of it
+wl_offset = 28 # in pixel, shift of the bandwidth on the detecotr hence a blank in a part of it
 detector_size = (344, 96) #resp. position axes and wavelength
 
 channel_width = bandwidth / (detector_size[1])
@@ -85,9 +58,14 @@ QE = 0.8
 gainsys = 0.5 # ADU/electron
 offset = 2750
 
-wl_scale = np.linspace(wl_min, wl_max, detector_size[1], endpoint=False)[::-1] #In microns
+wl_scale, step = np.linspace(wl_min, wl_max, detector_size[1], endpoint=False, retstep=True) #In microns
+wl_scale = wl_scale[::-1]
+
 wave_number = 1/wl_scale # In microns^-1
 position_scale = np.arange(0, detector_size[0]) #In pixel
+
+wl_scale_oversampled = np.array([np.linspace(wl_scale[i]-step/2, wl_scale[i]+step/2, 10, endpoint=False) for i in range(wl_scale.size)])
+wave_number_oversampled = 1/wl_scale_oversampled
 
 #==============================================================================
 # Properties of the pupils and the instrument
@@ -103,13 +81,6 @@ transmission = np.ones((nb_pupils,))*0.01
 # Properties of the integrated optics and injection
 # =============================================================================
 beam_comb = [str(i)+''+str(j) for i,j in combinations(np.arange(4), 2)]
-#beam_splitter = np.array([[0.37, 0.63], [0.43, 0.57], [0.5, 0.5], [0.5, 0.5]]) # one couple per beam. Toward photo and coupler
-beam_splitter = np.array([[0.5, 0.5], [0.5, 0.5], [0.5, 0.5], [0.5, 0.5]]) # one couple per beam. Toward photo and coupler
-beam_splitter = chromatic_splitters(beam_splitter, wl_scale, 1.55, 0.)
-
-# Setting the phase of the coupler
-couplers_splitter = np.ones((6,2)) * np.pi/4 # Toward null and anti-null
-couplers_splitter = chromatic_splitters(couplers_splitter, wl_scale, 1.55, 0.)
 
 rho0 = 0.8
 
@@ -117,15 +88,19 @@ rho0 = 0.8
 # Properties of atmosphere
 # =============================================================================
 
-opd0 = 1.552/4.
+opd0 = 1.602/4.
 wave_number0 = 1./(2*opd0) # wave number for which the null is minimal
-phase_bias = 0.#-np.pi/2 # Generic phase bias term
+phase_bias = np.array([-9.801769079200153278e-01, 1.130973355292325344e+00,
+                       -9.424777960769379348e-01, 3.129026282975434725e+00,
+                       -2.783451091080556772e+00, 1.394867138193868428e+00])#-np.pi/2 # Generic phase bias term
 
+piston_mean, piston_std, jump = 1.602/2, 0.1, 1.
+strehl_mean, strehl_std = 0.5, 0.12
 #==============================================================================
 # Stars
 #==============================================================================
-mag = 0.
-na = 0.01
+mag = 2.
+na = 0.0
 visibility = (1 - na) / (1. + na)
 
 # =============================================================================
@@ -134,8 +109,12 @@ visibility = (1 - na) / (1. + na)
 channel_positions = np.array([ 33.,  53.,  72.,  92., 112., 132., 151., 171., 191., 211., 230., 250., 270., 290., 309., 329.])
 spacing = 1. / (comb(nb_pupils, 2)*2+nb_pupils+1+2)
 photo_pos = [channel_positions[15], channel_positions[13], channel_positions[2], channel_positions[0]]
-null_output_pos = [channel_positions[11], channel_positions[3], channel_positions[1], channel_positions[6], channel_positions[5], channel_positions[8]]
-anti_null_output_pos = [channel_positions[9], channel_positions[12], channel_positions[14], channel_positions[4], channel_positions[7], channel_positions[10]]
+
+# Order of null:
+# 12, 13, 14, 23, 24, 34
+# N1, N5, N3, N2, N6, N4
+null_output_pos = [channel_positions[11], channel_positions[5], channel_positions[1], channel_positions[3], channel_positions[8], channel_positions[6]]
+anti_null_output_pos = [channel_positions[9], channel_positions[7], channel_positions[14], channel_positions[12], channel_positions[10], channel_positions[4]]
 sigma = 0.9
 
 # =============================================================================
@@ -149,13 +128,15 @@ photometric_channels = np.transpose(photometric_channels, [0, 2, 1])
 null_channels = np.transpose(null_channels, [0, 2, 1])
 anti_null_channels = np.transpose(anti_null_channels, [0, 2, 1])
 
+zeta_minus, zeta_plus = setZetaCoeff(wl_scale, '/mnt/96980F95980F72D3/glint/simulation/zeta_coeff2.hdf5', save=False)
+
 start0 = time()
 for bl in range(*nb_block):
     start = time()
     print("Generating block %s / %s"%(bl+1,nb_block[1]))
     if turbulence:
         np.random.seed()
-        strehl = np.random.normal(0.5, 0.12, size=(nb_pupils, nbimg))
+        strehl = np.random.normal(strehl_mean, strehl_std, size=(nb_pupils, nbimg))
         while np.min(strehl) < 0 or np.max(strehl) > 1:
             idx = np.where((strehl<0)|(strehl>1))
             strehl[idx] = np.random.normal(0.5, 0.12, size=idx[0].size)
@@ -165,49 +146,64 @@ for bl in range(*nb_block):
     rho = 0.8 * strehl
 
     if turbulence:
-        delta_opd = np.random.normal(0,0.4, (int(comb(nb_pupils, 2)), nbimg))
+#        delta_opd = np.random.normal(0, 5, (int(comb(nb_pupils, 2)), nbimg))
+        delta_opd = rv_gen_doubleGauss((int(comb(nb_pupils, 2)), nbimg), 0., piston_mean, piston_std, jump)
     else:
         delta_opd = np.zeros((int(comb(nb_pupils, 2)), nbimg))
         
-    opd = opd0 * np.ones(delta_opd.shape) + delta_opd*0
-    opd[4] = opd[0] + opd[1] # 13 = 12 + 23 i.e. N5
-    opd[5] = opd[1] + opd[3] # 24 = 23 + 34 i.e. N6
-    opd[2] = opd[4] + opd[3] # 14 = 13 + 34 i.e. N3
+    opd = opd0 * np.ones(delta_opd.shape) + delta_opd
+    opd[0] = (0.39999938011169434 - (-1.500000000000056843e-02)) * np.ones(delta_opd.shape[1]) + delta_opd[0] #N1 1st value: optimal piston on seg 29, 2nd value: offset of the center of coherent envelop
+    opd[3] = (0 - 9.653999999999999915e+00) * np.ones(delta_opd.shape[1]) + delta_opd[3] #N2
+    opd[5] = (-1.299999475479126 - (-2.721000000000000085e+00)) * np.ones(delta_opd.shape[1]) + delta_opd[5] #N4
+#    opd_ramp = np.linspace(-50,+50,nbimg, endpoint=False)
+#    opd[0,:] += opd_ramp
+    
+    # Order of pairs of beams: 12, 13, 14, 23, 24, 34
+    opd[1] = opd[0] + opd[3] # 13 = 12 + 23 i.e. N5
+    opd[2] = opd[1] + opd[5] # 14 = 13 + 34 i.e. N3
+    opd[4] = opd[3] + opd[5] # 24 = 23 + 34 i.e. N6
+    
     image_clean = np.zeros((nbimg, detector_size[0], detector_size[1]))
     
     if not dark_only:
         idx_count = 0
         for i in liste_pupils:
-            N = np.array([QE * surface * channel_width * DIT * np.power(10, -0.4*mag) * flux_ref * wl_scale.mean()*1.E-6 /(h*c)]*detector_size[1])
+            N = QE * surface * channel_width * DIT * np.power(10, -0.4*mag) * flux_ref * wl_scale.mean()*1.E-6 /(h*c) * np.ones(detector_size[1])
             photo = np.zeros(image_clean.shape)
-            photo[:] = beam_splitter[i][0] * transmission[i] * photometric_channels[i] * N[None, :] * rho[i,:,None,None]
+            photo[:] = transmission[i] * photometric_channels[i] * N[None, :] * rho[i,:,None,None]
             
             image_clean += photo
             
             for j in liste_pupils[i+1:]:
-                kappa_l = couplers_splitter[idx_count,0,:] # Asymmetric coupler is not implemented yet
-                
                 beam_i_null = np.zeros(image_clean.shape)
                 beam_i_anti_null = np.zeros(image_clean.shape)
                 beam_j_null = np.zeros(image_clean.shape)
                 beam_j_anti_null = np.zeros(image_clean.shape)
                 
-                beam_i_null[:] = beam_splitter[i][1] * transmission[i] * null_channels[idx_count] * N[None,:] * rho[i,:,None,None]
-                beam_i_anti_null[:] = beam_splitter[i][1] * transmission[i] * anti_null_channels[idx_count] * N[None,:] * rho[i,:,None,None]
+                zeta_minus_i = zeta_minus[idx_count,0]
+                zeta_minus_j = zeta_minus[idx_count,1]
+                zeta_plus_i = zeta_plus[idx_count,0]
+                zeta_plus_j = zeta_plus[idx_count,1]
                 
-                beam_j_null[:] = beam_splitter[j][1] * transmission[j] * null_channels[idx_count] * N[None,:] * rho[j,:,None,None]
-                beam_j_anti_null[:] = beam_splitter[j][1] * transmission[j] * anti_null_channels[idx_count] * N[None,:] * rho[j,:,None,None]
+                beam_i_null[:] = transmission[i] * null_channels[idx_count] * N[None,:] * rho[i,:,None,None]
+                beam_i_anti_null[:] = transmission[i] * anti_null_channels[idx_count] * N[None,:] * rho[i,:,None,None]
+                
+                beam_j_null[:] = transmission[j] * null_channels[idx_count] * N[None,:] * rho[j,:,None,None]
+                beam_j_anti_null[:] = transmission[j] * anti_null_channels[idx_count] * N[None,:] * rho[j,:,None,None]
                 
                 beams_ij_null = beam_i_null * beam_j_null
                 beams_ij_anti_null = beam_i_anti_null * beam_j_anti_null
                 
-                sine = np.array([np.sin(2*np.pi*wave_number*d + phase_bias) for d in opd[idx_count]])
-                null_output = beam_i_null * np.sin(kappa_l)**2 + beam_j_null * np.cos(kappa_l)**2 -\
-                np.sqrt(beams_ij_null) * np.sin(2 * kappa_l) * visibility * sine[:,None,:]
+
+#                sine = np.array([np.sin(2*np.pi*wave_number*d + phase_bias) for d in opd[idx_count]])
+                sine = np.array([np.sin(2*np.pi*wave_number_oversampled*d + phase_bias[idx_count]) for d in opd[idx_count]])
+                sine = np.mean(sine, axis=2)
+                null_output = beam_i_null * zeta_minus_i + beam_j_null * zeta_minus_j -\
+                2*np.sqrt(beams_ij_null) * np.sqrt(zeta_minus_i * zeta_minus_j) * visibility * sine[:,None,:]
                 
-                anti_null_output = beam_i_anti_null * np.cos(kappa_l)**2 + beam_j_anti_null * np.sin(kappa_l)**2 + \
-                np.sqrt(beams_ij_anti_null) * np.sin(2 * kappa_l) * visibility * sine[:,None,:]
-                
+                anti_null_output = beam_i_anti_null * zeta_plus_i + beam_j_anti_null * zeta_plus_j + \
+                2*np.sqrt(beams_ij_anti_null) * np.sqrt(zeta_plus_i * zeta_plus_j) * visibility * sine[:,None,:]
+
                 image_clean += null_output + anti_null_output
                 idx_count += 1
         
@@ -233,7 +229,7 @@ for bl in range(*nb_block):
             path2 = path + 'dark_%04d.mat'%(bl+1)
         
         date = datetime.datetime.utcnow().isoformat()
-        save(np.transpose(data, axes=(0,2,1)), path2, date, DIT, na, mag)
+        save(np.transpose(data, axes=(0,2,1)), path2, date, DIT, na, mag, nbimg, [piston_mean, piston_std, jump], [strehl_mean, strehl_std])
     
     stop = time()
     print('Last: %.3f'%(stop-start))
@@ -244,11 +240,12 @@ print('Total last: %.3f'%(stop0-start0))
 # =============================================================================
 # Miscelleanous
 # =============================================================================
-null = image_clean[:,int(round(null_output_pos[0])),:]
-antinull = image_clean[:,int(round(anti_null_output_pos[0])),:]
-p = np.array([image_clean[:,int(round(photo_pos_i)),:] for photo_pos_i in photo_pos])
+output = data
+null = output[:,int(round(null_output_pos[0])),:]
+antinull = output[:,int(round(anti_null_output_pos[0])),:]
+p = np.array([output[:,int(round(photo_pos_i)),:] for photo_pos_i in photo_pos])
 
-for i in range(min(5, nbimg)):
+for i in range(min(5,nbimg)):
     plt.figure()
     plt.subplot(131)
     plt.imshow(data[i], interpolation='none', aspect='auto', extent=[wl_scale[0], wl_scale[-1], detector_size[0], 0],) 
@@ -266,18 +263,34 @@ for i in range(min(5, nbimg)):
     plt.grid()
     plt.legend(loc='best')
 
-null_fft = np.fft.fftshift(np.fft.fft((null), norm='ortho'))
-anti_fft = np.fft.fftshift(np.fft.fft((antinull), norm='ortho'))
+plt.figure()
+plt.plot(opd[0,:], null[:,55], label='null')
+plt.grid()
+plt.legend(loc='best')
 
-null_dsp, null_phase = abs(null_fft)**2, np.angle(null_fft)
-anti_dsp, anti_phase = abs(anti_fft)**2, np.angle(anti_fft)
+plt.figure()
+plt.plot(opd[0,:], null[:,20:60].sum(axis=1), '-', label='sum of null')
+plt.grid()
+plt.xlim(-50,50)
+plt.legend(loc='best')
 
-#plt.figure()
-#plt.plot(image_clean[:,329,:].sum(axis=-1)/np.max(image_clean[:,329,:].sum(axis=-1))) # Photo
-#plt.plot(image_clean[:,191,:].sum(axis=-1)/np.max(image_clean[:,191,:].sum(axis=-1))) #Null
-#plt.plot(rho[0]/rho[0].max(), 'o')
-#plt.grid()
-#
-#plt.figure()
-#plt.plot(anti_phase[0])
-#plt.grid()
+from matplotlib import animation
+
+fig = plt.figure()
+ax = plt.axes()
+time_text = ax.text(0.05, 0.01, '', transform=ax.transAxes, color='w')
+
+im = plt.imshow(output[0],interpolation='none')
+# initialization function: plot the background of each frame
+def init():
+    im.set_data(output[0])
+    time_text.set_text('')
+    return [im] + [time_text]
+
+# animation function.  This is called sequentially
+def animate(i):
+    im.set_array(output[i])
+    time_text.set_text('Frame %s/%s'%(i+1, image_clean.shape[0]))
+    return [im] + [time_text]
+
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=output.shape[0], interval=200, blit=True)
